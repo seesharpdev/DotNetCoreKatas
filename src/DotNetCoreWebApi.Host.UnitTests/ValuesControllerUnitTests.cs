10645b0a0ed73b01453676b22edd29a53638ff9e
Carlos Souto <carlos.p.souto@hotmail.com>
2018-07-22 12:52:41 +0100

    Extend Unit Tests and add README.MD;

--- src/DotNetCoreWebApi.Host.UnitTests/ValuesControllerUnitTests.cs
+++ src/DotNetCoreWebApi.Host.UnitTests/ValuesControllerUnitTests.cs
@@ -12,32 +12,167 @@ namespace DotNetCoreWebApi.Host.UnitTests
 	[ExcludeFromCodeCoverage]
     public class ValuesControllerUnitTests
     {
-        [Fact]
-        public void Get_WhenCalled_ReturnsActionResult()
-        {
-			// Arrange
-	        var controller = new ValuesController();
+	    private static class Factory
+	    {
+		    internal static ValuesController New()
+		    {
+				return new ValuesController();
+		    }
+	    }
 
-	        // Act
-	        var result = controller.Get();
+	    [Fact]
+	    public void Get_WhenCalled_ReturnsOkObjectResult()
+	    {
+		    // Arrange
+		    var controller = Factory.New();
 
-	        // Assert
-	        Assert.IsType<ActionResult<IEnumerable<string>>>(result);
-			Assert.IsAssignableFrom<IEnumerable<string>>(result.Value);
-		}
+		    // Act
+		    var response = controller.Get();
+
+		    // Assert
+		    Assert.IsType<ActionResult<IEnumerable<string>>>(response);
+		    Assert.IsAssignableFrom<OkObjectResult>(response.Result);
+	    }
 
 	    [Fact]
 	    public void Get_WhenCalled_ReturnsAllItems()
 	    {
-		    // Arrange
-		    var controller = new ValuesController();
+			// Arrange
+		    var controller = Factory.New();
 
-		    // Act
-		    var result = controller.Get();
+			// Act
+			var response = controller.Get().Result as OkObjectResult;
 
 		    // Assert
-		    var items = Assert.IsAssignableFrom<IEnumerable<string>>(result.Value);
-			Assert.Equal(2, items.Count());
+		    var items = Assert.IsAssignableFrom<IEnumerable<string>>(response.Value);
+		    Assert.Equal(2, items.Count());
+	    }
+
+	    [Fact]
+	    public void GetById_UnknownIdPassed_ReturnsNotFoundResult()
+	    {
+			// Arrange
+		    var controller = Factory.New();
+
+			// Act
+		    var notFoundResult = controller.Get(0);
+
+		    // Assert
+		    Assert.IsType<NotFoundResult>(notFoundResult.Result);
+	    }
+
+	    [Fact]
+	    public void GetById_ExistingIdPassed_ReturnsOkResult()
+	    {
+			// Arrange
+		    var controller = Factory.New();
+		    var testId = 1;
+
+		    // Act
+		    var okResult = controller.Get(testId);
+
+		    // Assert
+		    Assert.IsType<OkObjectResult>(okResult.Result);
+	    }
+
+	    [Fact]
+	    public void GetById_ExistingIdPassed_ReturnsRightItem()
+	    {
+			// Arrange
+		    var controller = Factory.New();
+		    var testId = 1;
+
+		    // Act
+		    var okResult = controller.Get(testId).Result as OkObjectResult;
+
+		    // Assert
+		    Assert.IsType<string>(okResult.Value);
+		    Assert.Equal("value", okResult.Value as string);
+	    }
+
+	    [Fact]
+	    public void Add_InvalidObjectPassed_ReturnsBadRequest()
+	    {
+			// Arrange
+		    var controller = Factory.New();
+			controller.ModelState.AddModelError("Value", "Required");
+
+		    // Act
+		    var badResponse = controller.Post(string.Empty);
+
+		    // Assert
+		    Assert.IsType<BadRequestObjectResult>(badResponse);
+	    }
+
+	    [Fact]
+	    public void Add_ValidObjectPassed_ReturnsCreatedResponse()
+	    {
+			// Arrange
+		    var controller = Factory.New();
+
+		    // Act
+		    var createdResponse = controller.Post("testValue");
+
+		    // Assert
+		    Assert.IsType<CreatedAtActionResult>(createdResponse);
+	    }
+
+	    [Fact]
+	    public void Add_ValidObjectPassed_ReturnedResponseHasCreatedItem()
+	    {
+			// Arrange
+			var controller = Factory.New();
+		    var testValue = "testValue";
+
+		    // Act
+		    var createdResponse = controller.Post(testValue) as CreatedAtActionResult;
+		    var item = createdResponse.Value as string;
+
+		    // Assert
+		    Assert.IsType<string>(item);
+			Assert.Equal(testValue, item);
 		}
-    }
+
+	    [Fact]
+	    public void Remove_NotExistingGuidPassed_ReturnsNotFoundResponse()
+	    {
+			// Arrange
+		    var controller = Factory.New();
+			const int notExistingId = 0;
+
+		    // Act
+		    var badResponse = controller.Delete(notExistingId);
+
+		    // Assert
+		    Assert.IsType<NotFoundResult>(badResponse);
+	    }
+
+	    [Fact]
+	    public void Remove_ExistingGuidPassed_ReturnsOkResult()
+	    {
+			// Arrange
+		    var controller = Factory.New();
+			const int existingId = 1;
+
+		    // Act
+		    var okResponse = controller.Delete(existingId);
+
+		    // Assert
+		    Assert.IsType<OkResult>(okResponse);
+	    }
+
+		[Fact(Skip = "Service need to be mocked and expectations verified.")]
+		public void Remove_ExistingGuidPassed_RemovesOneItem()
+	    {
+			// Arrange
+		    var controller = Factory.New();
+			const int existingId = 1;
+
+		    // Act
+		    var okResponse = controller.Delete(existingId);
+
+			// Assert
+			//Assert.Equal(2, _service.GetAllItems().Count());
+		}
+	}
 }
--- src/DotNetCoreWebApi.Host/Controllers/ValuesController.cs
+++ src/DotNetCoreWebApi.Host/Controllers/ValuesController.cs
@@ -12,32 +12,55 @@ namespace DotNetCoreWebApi.Host.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+	        var strings = new[] { "value1", "value2" };
+
+	        return Ok(strings);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+	        const string value = "value";
+	        if (id == 0)
+	        {
+		        return NotFound();
+	        }
+
+	        return Ok(value);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] string value)
         {
-        }
+	        if (!ModelState.IsValid)
+	        {
+		        return BadRequest(ModelState);
+	        }
+
+			return CreatedAtAction("Get", new { id = value }, value);
+		}
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
-        }
+	        //var existingItem = _service.GetById(id);
+	        if (id == 0)
+	        {
+		        return NotFound();
+	        }
+
+	        //_service.Remove(id);
+	        return Ok();
+		}
     }
 }
