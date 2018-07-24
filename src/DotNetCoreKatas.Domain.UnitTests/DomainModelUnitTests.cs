92ba154bd5124967c3325b23644c9034171734be
Carlos Souto <carlos.p.souto@hotmail.com>
2018-07-25 00:23:12 +0100

    Add Book Domain Model;

--- src/DotNetCoreKatas.Domain.UnitTests/DomainModelUnitTests.cs
+++ src/DotNetCoreKatas.Domain.UnitTests/DomainModelUnitTests.cs
@@ -3,7 +3,6 @@ using System.Reflection;
 using Xunit;
 
 using DotNetCoreKatas.Core.Domain;
-using DotNetCoreKatas.Core.Interfaces;
 using DotNetCoreKatas.Domain.Models;
 
 namespace DotNetCoreKatas.Domain.UnitTests
@@ -12,41 +11,32 @@ namespace DotNetCoreKatas.Domain.UnitTests
     {
 	    private static class Factory
 	    {
-		    internal static IAggregateRoot<int> New(int id)
+		    internal static DomainModel New(int id)
 		    {
-			    var domainModel = DomainModel<int>.Factory.New(id);
+			    var domainModel = new DomainModel(id);
 
 				return domainModel;
 		    }
 	    }
 
 	    [Fact]
-	    public void DomainModel_Should_Have_Private_Ctor()
+	    public void DomainModel_Should_Have_Protected_Ctor()
 	    {
 			// arrange
 		    const BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.Static | 
 				BindingFlags.NonPublic | BindingFlags.Instance;
 
 		    // Act
-		    ConstructorInfo[] ctorInfo = typeof(DomainModel<>).GetConstructors(bindingFlags);
+		    ConstructorInfo[] ctorInfo = typeof(DomainModel).GetConstructors(bindingFlags);
 
 			// Assert
-			Assert.Single(ctorInfo);
-			Assert.True(ctorInfo[0].IsPrivate);
-		}
-
-	    [Fact]
-	    public void DomainModel_Should_Be_Instantiated_By_The_Factory()
-	    {
-		    // Act
-		    var domainModel = Factory.New(1);
-
-		    // Assert
-			Assert.NotNull(domainModel);
+			Assert.Equal(2, ctorInfo.Length);
+			Assert.Contains(ctorInfo, c => c.IsPublic);
+			Assert.Contains(ctorInfo, c => c.IsPrivate);
 		}
 
 		[Fact]
-        public void DomainModel_Should_Inherit_From_AggregateRoot()
+		public void DomainModel_Should_Inherit_From_AggregateRoot()
         {
 			// Act
 			var domainModel = Factory.New(1);
---
+++ src/DotNetCoreKatas.Domain/Models/BookDomainModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using DotNetCoreKatas.Core.Interfaces;
+
+namespace DotNetCoreKatas.Domain.Models
+{
+	public class BookDomainModel : IAggregateRoot<int>
+	{
+		#region Ctor's
+
+		/// <summary>
+		/// This should be our only option to instantiate a new Book AR.
+		/// </summary>
+		/// <param name="id"></param>
+		public BookDomainModel(int id)
+		{
+			Id = id;
+		}
+
+		/// <summary>
+		/// Needed by EF Core.
+		/// </summary>
+		// ReSharper disable once UnusedMember.Local
+		private BookDomainModel()
+		{
+		}
+
+		#endregion
+
+		[Key]
+		public int Id { get; }
+	}
+}
