ea15227ef6b31aa4dffaad7450051d6f50c30d2f
Carlos Souto <carlos.p.souto@hotmail.com>
2018-07-25 14:18:07 +0100

    Update Persistence layer;

--- src/DotNetCoreKatas.Persistence/Extensions/QueryableExtensions.cs
+++ src/DotNetCoreKatas.Persistence/Extensions/QueryableExtensions.cs
@@ -5,9 +5,12 @@ using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace DotNetCoreKatas.Persistence.Extensions
 {
+	/// <summary>
+	/// Custom implementation of 'AsNoTracking' to facilitate testing of classes depending on <see cref="DbSet{TEntity}"/>
+	/// </summary>
 	public static class QueryableExtensions
 	{
-		public static IQueryable<T> GatedAsNoTracking<T>(this IQueryable<T> source) where T : class
+		public static IQueryable<T> AsNoTrackingQueryable<T>(this IQueryable<T> source) where T : class
 		{
 			if (source.Provider is EntityQueryProvider)
 			{
