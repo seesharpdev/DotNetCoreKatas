518839baad083a19ba9864055bc6077ce5ed0960
Carlos Souto <carlos.p.souto@hotmail.com>
2018-07-25 14:15:30 +0100

    Make .Net Standard 2 the target framework for neutral assemblies;

--- src/DotNetCoreKatas.Core/Interfaces/IAggregateRoot.cs
+++ src/DotNetCoreKatas.Core/Interfaces/IAggregateRoot.cs
@@ -1,5 +1,9 @@
 ﻿namespace DotNetCoreKatas.Core.Interfaces
 {
+	/// <summary>
+	/// Represents a Bounded Context Aggregate Root contract.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public interface IAggregateRoot<out T>
 	{
 		T Id { get; }
--- src/DotNetCoreKatas.Domain/Models/BookDomainModel.cs
+++ src/DotNetCoreKatas.Domain/Models/BookDomainModel.cs
@@ -1,33 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-using DotNetCoreKatas.Core.Interfaces;
+﻿using DotNetCoreKatas.Core.Interfaces;
 
 namespace DotNetCoreKatas.Domain.Models
 {
 	public class BookDomainModel : IAggregateRoot<int>
 	{
-		#region Ctor's
+		#region Private Members
 
+		private int _id;
+
+		#endregion
+
+		/// <inheritdoc />
 		/// <summary>
 		/// This should be our only option to instantiate a new Book AR.
 		/// </summary>
 		/// <param name="id"></param>
 		public BookDomainModel(int id)
+			: this()
 		{
-			Id = id;
+			_id = id;
 		}
 
 		/// <summary>
 		/// Needed by EF Core.
 		/// </summary>
-		// ReSharper disable once UnusedMember.Local
 		private BookDomainModel()
 		{
 		}
 
-		#endregion
-
-		[Key]
-		public int Id { get; }
+		public int Id => _id;
 	}
 }
