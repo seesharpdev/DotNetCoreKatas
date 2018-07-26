458a3f5a21e9363f8ecb6c2356e87d3342db6f89
Carlos Souto <carlos.p.souto@hotmail.com>
2018-07-26 21:21:43 +0100

    Add FindBookQueryHandler and Unit Tests

---
+++ src/DotNetCoreKatas.QueryAdapter/Handlers/FindBookQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using DotNetCoreKatas.Core.Interfaces;
+using DotNetCoreKatas.Core.Interfaces.Querying;
+using DotNetCoreKatas.Domain.Models;
+using DotNetCoreKatas.Persistence;
+using DotNetCoreKatas.Query.Contracts.Models;
+using DotNetCoreKatas.Query.Contracts.Queries;
+
+namespace DotNetCoreKatas.Query.Adapter.Handlers
+{
+	public class FindBookQueryHandler : IQueryHandler<FindBookQuery, BookReadModel>
+	{
+		private readonly IDotNetCoreKatasDbContext _dbContext;
+		private readonly IModelMapper<BookDomainModel, BookReadModel> _mapper;
+
+		public FindBookQueryHandler(
+			IDotNetCoreKatasDbContext dbContext,
+			IModelMapper<BookDomainModel, BookReadModel> mapper)
+		{
+			_dbContext = dbContext;
+			_mapper = mapper;
+		}
+
+		public BookReadModel Handle(FindBookQuery query)
+		{
+			var model = _dbContext.Books.Find(query.Predicate);
+			var book = _mapper.Map(model);
+
+			return book;
+		}
+	}
+}
