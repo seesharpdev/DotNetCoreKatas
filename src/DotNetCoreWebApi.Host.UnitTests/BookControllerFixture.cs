b7c37fe9303770e217647b2d1cd35d48464ab039
Carlos Souto <carlos.p.souto@hotmail.com>
2018-07-27 02:17:09 +0100

    Make use of AutoMock in BooksController Unit Tests;

---
+++ src/DotNetCoreWebApi.Host.UnitTests/BookControllerFixture.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Moq;
+
+using DotNetCoreKatas.Command.Adapter.Contracts;
+using DotNetCoreKatas.Core.Interfaces.Querying;
+using DotNetCoreKatas.Query.Contracts.Models;
+
+namespace DotNetCoreWebApi.Host.UnitTests
+{
+	public class BookControllerFixture : IDisposable
+	{
+		public BookControllerFixture()
+		{
+			ReadModels = new[]
+				{
+					new BookReadModel { Id = 1 }
+				};
+		}
+
+	    public IEnumerable<BookReadModel> ReadModels { get; }
+
+		public void Dispose()
+	    {
+	    }
+    }
+}
--- src/DotNetCoreWebApi.Host.UnitTests/BooksControllerUnitTests.cs
+++ src/DotNetCoreWebApi.Host.UnitTests/BooksControllerUnitTests.cs
@@ -3,188 +3,235 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Autofac.Extras.Moq;
 using Microsoft.AspNetCore.Mvc;
 using Xunit;
 using Moq;
 
 using DotNetCoreKatas.Command.Adapter.Contracts;
 using DotNetCoreKatas.Command.Contracts;
-using DotNetCoreKatas.Core.Interfaces.Querying;
 using DotNetCoreKatas.Query.Contracts.Models;
 using DotNetCoreWebApi.Host.Controllers;
+using DotNetCoreKatas.Core.Interfaces.Querying;
 
 namespace DotNetCoreWebApi.Host.UnitTests
 {
 	[ExcludeFromCodeCoverage]
-    public class BooksControllerUnitTests
+    public class BooksControllerUnitTests : IClassFixture<BookControllerFixture>
 	{
-		private static readonly Mock<IQueryAdapter<BookReadModel, int>> QueryAdapterMock = new Mock<IQueryAdapter<BookReadModel, int>>();
-		private static readonly Mock<IBooksCommandAdapter> CommandAdapterMock = new Mock<IBooksCommandAdapter>();
-
-		private static class Factory
-	    {
-			internal static BooksController New()
-			{
-				IEnumerable<BookReadModel> bookReadModels = new[] { new BookReadModel { Id = 1 } };
-
-				QueryAdapterMock.Setup(_ => _.GetAll())
-					.Returns(Task.FromResult(bookReadModels));
-
-				QueryAdapterMock.Setup(_ => _.GetById(It.IsAny<int>()))
-					.ReturnsAsync(bookReadModels.FirstOrDefault);
-
-				return new BooksController(QueryAdapterMock.Object, CommandAdapterMock.Object);
-		    }
-	    }
+		private readonly BookControllerFixture _fixture;
+		
+		public BooksControllerUnitTests(BookControllerFixture fixture)
+		{
+			_fixture = fixture;
+		}
 
 	    [Fact]
 	    public void Get_WhenCalled_ReturnsOkObjectResult()
 	    {
-		    // Arrange
-		    var controller = Factory.New();
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+			    mock.Mock<IQueryAdapter<BookReadModel, int>>()
+				    .Setup(_ => _.GetAll())
+				    .Returns(Task.FromResult(_fixture.ReadModels));
 
-		    // Act
-		    var response = controller.Get().Result as OkObjectResult;
+				var controller = mock.Create<BooksController>();
 
-			// Assert
-			Assert.IsAssignableFrom<OkObjectResult>(response);
-			Assert.IsAssignableFrom<IEnumerable<BookReadModel>>(response.Value);
+				// Act
+				var response = controller.Get().Result as OkObjectResult;
+
+			    // Assert
+			    Assert.IsAssignableFrom<OkObjectResult>(response);
+			    Assert.IsAssignableFrom<IEnumerable<BookReadModel>>(response.Value);
+		    }
 		}
 
 		[Fact]
 	    public void Get_WhenCalled_ReturnsAllItems()
 	    {
-			// Arrange
-		    var controller = Factory.New();
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+				mock.Mock<IQueryAdapter<BookReadModel, int>>()
+					.Setup(_ => _.GetAll())
+					.Returns(Task.FromResult(_fixture.ReadModels));
 
-			// Act
-			var response = controller.Get().Result as OkObjectResult;
+				var controller = mock.Create<BooksController>();
 
-			// Assert
-			var items = Assert.IsAssignableFrom<IEnumerable<BookReadModel>>(response?.Value);
-		    Assert.Single(items);
+				// Act
+				var response = controller.Get().Result as OkObjectResult;
+
+			    // Assert
+			    var items = Assert.IsAssignableFrom<IEnumerable<BookReadModel>>(response?.Value);
+			    Assert.Single(items);
+			    mock.Mock<IQueryAdapter<BookReadModel, int>>()
+				    .VerifyAll();
+			}
 	    }
 
 	    [Fact]
 	    public void GetById_UnknownIdPassed_ReturnsNotFoundResult()
 	    {
-			// Arrange
-		    var controller = Factory.New();
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+				var controller = mock.Create<BooksController>();
 
-			// Act
-		    var result = controller.Get(0).Result;
+				// Act
+				var result = controller.Get(0).Result;
 
-		    // Assert
-		    Assert.IsType<NotFoundResult>(result);
+			    // Assert
+			    Assert.IsType<NotFoundResult>(result);
+		    }
 	    }
 
 	    [Fact]
 	    public void GetById_ExistingIdPassed_ReturnsOkResult()
 	    {
-			// Arrange
-		    var controller = Factory.New();
-		    const int testId = 1;
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+			    mock.Mock<IQueryAdapter<BookReadModel, int>>()
+				    .Setup(_ => _.GetById(It.IsAny<int>()))
+				    .ReturnsAsync(_fixture.ReadModels.FirstOrDefault);
 
-		    // Act
-		    var result = controller.Get(testId).Result;
+				var controller = mock.Create<BooksController>();
+				const int testId = 1;
 
-		    // Assert
-		    Assert.IsType<OkObjectResult>(result);
+			    // Act
+			    var result = controller.Get(testId).Result;
+
+			    // Assert
+			    Assert.IsType<OkObjectResult>(result);
+			    mock.Mock<IQueryAdapter<BookReadModel, int>>()
+				    .VerifyAll();
+			}
 	    }
 
 	    [Fact]
 	    public void GetById_ExistingIdPassed_ReturnsRightItem()
 	    {
-			// Arrange
-		    var controller = Factory.New();
-		    const int modelId = 1;
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+			    mock.Mock<IQueryAdapter<BookReadModel, int>>()
+				    .Setup(_ => _.GetById(It.IsAny<int>()))
+				    .ReturnsAsync(_fixture.ReadModels.FirstOrDefault);
 
-		    // Act
-		    var result = controller.Get(modelId).Result as OkObjectResult;
+				var controller = mock.Create<BooksController>();
+				const int modelId = 1;
 
-		    // Assert
-		    Assert.IsType<BookReadModel>(result?.Value);
-		    Assert.Equal(modelId, (result.Value as BookReadModel).Id);
+			    // Act
+			    var result = controller.Get(modelId).Result as OkObjectResult;
+
+			    // Assert
+			    Assert.IsType<BookReadModel>(result?.Value);
+			    Assert.Equal(modelId, (result.Value as BookReadModel).Id);
+			    mock.Mock<IQueryAdapter<BookReadModel, int>>()
+				    .VerifyAll();
+			}
 	    }
 
 	    [Fact]
 	    public void Post_InvalidObjectPassed_ReturnsBadRequest()
 	    {
-			// Arrange
-		    var controller = Factory.New();
-			controller.ModelState.AddModelError("Value", "Required");
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+				var controller = mock.Create<BooksController>();
+				controller.ModelState.AddModelError("Value", "Required");
 
-		    // Act
-		    var response = controller.Post(null);
+			    // Act
+			    var response = controller.Post(null);
 
-		    // Assert
-		    Assert.IsType<BadRequestObjectResult>(response);
+			    // Assert
+			    Assert.IsType<BadRequestObjectResult>(response);
+		    }
 	    }
 
 	    [Fact]
 	    public void Post_ValidObjectPassed_ReturnsCreatedResponse()
 	    {
-			// Arrange
-		    var controller = Factory.New();
-		    var model = new BookReadModel { Id = 1 };
-		    CommandAdapterMock.Setup(_ => _.CreateBook(It.IsAny<ICreateBookCommand>()))
-			    .Returns(Task.CompletedTask);
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+			    mock.Mock<IBooksCommandAdapter>()
+				    .Setup(_ => _.CreateBook(It.IsAny<ICreateBookCommand>()))
+				    .Returns(Task.CompletedTask);
 
-			// Act
-			var response = controller.Post(model);
+			    var controller = mock.Create<BooksController>();
+			    var model = new BookReadModel { Id = 1 };
 
-		    // Assert
-		    Assert.IsType<CreatedAtActionResult>(response);
-			CommandAdapterMock.VerifyAll();
+			    // Act
+			    var response = controller.Post(model);
+
+			    // Assert
+			    Assert.IsType<CreatedAtActionResult>(response);
+			    mock.Mock<IBooksCommandAdapter>().VerifyAll();
+		    }
 	    }
 
 	    [Fact]
 	    public void Post_ValidObjectPassed_ReturnedResponseHasCreatedItem()
 	    {
-			// Arrange
-			var controller = Factory.New();
-		    var model = new BookReadModel { Id = 1 };
-		    CommandAdapterMock.Setup(_ => _.CreateBook(It.IsAny<ICreateBookCommand>()))
-			    .Returns(Task.CompletedTask);
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+			    mock.Mock<IBooksCommandAdapter>()
+				    .Setup(_ => _.CreateBook(It.IsAny<ICreateBookCommand>()))
+				    .Returns(Task.CompletedTask);
 
-			// Act
-			var response = controller.Post(model) as CreatedAtActionResult;
-		    var book = response?.Value as BookReadModel;
+				var controller = mock.Create<BooksController>();
+				var model = new BookReadModel { Id = 1 };
 
-			// Assert
-			Assert.IsType<BookReadModel>(book);
-			Assert.NotNull(book);
+			    // Act
+			    var response = controller.Post(model) as CreatedAtActionResult;
+			    var book = response?.Value as BookReadModel;
+
+			    // Assert
+			    Assert.IsType<BookReadModel>(book);
+			    Assert.NotNull(book);
+			    mock.Mock<IBooksCommandAdapter>().VerifyAll();
+			}
 		}
 
 	    [Fact]
 	    public void Remove_NotExistingIdPassed_ReturnsNotFoundResponse()
 	    {
-			// Arrange
-		    var controller = Factory.New();
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+				var controller = mock.Create<BooksController>();
 
-		    // Act
-		    var response = controller.Delete(0);
+				// Act
+				var response = controller.Delete(0);
 
-		    // Assert
-		    Assert.IsType<NotFoundResult>(response);
+			    // Assert
+			    Assert.IsType<NotFoundResult>(response);
+		    }
 	    }
 
 	    [Fact]
 	    public void Remove_ExistingGuidPassed_ReturnsOkResult()
 	    {
-			// Arrange
-		    var controller = Factory.New();
-			const int existingId = 1;
-		    CommandAdapterMock.Setup(_ => _.DeleteBook(It.IsAny<IDeleteBookCommand>()))
-			    .Returns(Task.CompletedTask);
+		    using (var mock = AutoMock.GetStrict())
+		    {
+				// Arrange
+			    mock.Mock<IBooksCommandAdapter>()
+				    .Setup(_ => _.DeleteBook(It.IsAny<IDeleteBookCommand>()))
+				    .Returns(Task.CompletedTask);
 
-		    // Act
-		    var response = controller.Delete(existingId);
+				var controller = mock.Create<BooksController>();
+				const int existingId = 1;
 
-		    // Assert
-		    Assert.IsType<OkResult>(response);
-		    CommandAdapterMock.VerifyAll();
+			    // Act
+			    var response = controller.Delete(existingId);
 
+			    // Assert
+			    Assert.IsType<OkResult>(response);
+			    mock.Mock<IBooksCommandAdapter>().VerifyAll();
+		    }
 		}
 	}
 }
